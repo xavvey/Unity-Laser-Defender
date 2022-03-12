5578f985b12563a39d116f272d87a6d895ad7ec6
Xavier <xavier.veyrier@gmail.com>
2022-03-12 08:50:56 +0100

    init commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float paddingLeft = 0.6f;
+    [SerializeField] float paddingRight = 0.6f;
+    [SerializeField] float paddingTop = 2f;
+    [SerializeField] float paddingBottom = 3f;
+
+    Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        initBounds();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void initBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2();
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+
+        transform.position = newPosition;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+    }
+}
